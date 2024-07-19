85d05141c2bbc651747a5b430fb44c3e6cb846d9
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-19 17:28:04 +0900

    許可するキーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     sign_in @user # サインイン処理
+  #     redirect_to items_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday)
+  end
+
 end
