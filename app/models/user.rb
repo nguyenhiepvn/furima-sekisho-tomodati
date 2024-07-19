05fefa9b9d54eacb8dfd5a6338210916ce06f449
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-19 13:25:37 +0900

    モデルの設定
    アソシエーション・バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # アソシエーション
+  has_many :items
+  has_many :orders
+  has_many :comments
 
   # バリデーション
+  validates :nickname,            presence: true
+  validates :email,               presence: true
+  validates :encrypted_pasword,   presence: true
+  validates :first_name,          presence: true
+  validates :last_name,           presence: true
+  validates :first_name_kana,     presence: true
+  validates :last_name_kana,      presence: true
+  validates :birthday,            presence: true
 
 end
