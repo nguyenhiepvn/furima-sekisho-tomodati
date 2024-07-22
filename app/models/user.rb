77e2def69457af574762152884856923c9746d53
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-22 16:17:13 +0900

    アソシエーションの削除 正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,14 @@ class User < ApplicationRecord
 
   # アソシエーション
   has_many :items
-  has_many :orders
-  has_many :comments
 
   # バリデーション
   validates :nickname, presence: true
   validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/}, confirmation: true
   validates :birthday, presence: true
   validates :encrypted_password, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
 
