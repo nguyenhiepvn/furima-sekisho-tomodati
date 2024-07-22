b0786ed73282f3070c81ea9ecab3501fff8e52e9
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-22 11:10:15 +0900

    devise ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  devise_for :users
+    devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
   
   root "items#index"
   resources :users
