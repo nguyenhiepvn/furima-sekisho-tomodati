8ac9f9cfb5bd7f9060d5f92ba68d1e5bb4b62693
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-22 15:13:21 +0900

    新規登録後　リダイレクト

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,15 +6,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      sign_in(@user) # ユーザーをサインインさせます
+      redirect_to after_sign_up_path_for(@user) # サインアップ後のリダイレクト先を指定
     else
       render :new
     end
   end
+
+  protected
+
+  # サインアップ後のリダイレクト先を指定
+  def after_sign_up_path_for(resource)
+    root_path # 遷移先のパスを指定
+  end
+
   private
 
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday)
-    end
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday)
+  end
 
 end
