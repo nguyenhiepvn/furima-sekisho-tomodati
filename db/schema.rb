f9fd8dd12a332c1594a523897cb388554c7d0ab9
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-24 14:07:27 +0900

    itemモデル 作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_18_015757) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_24_020411) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.string "category_id", null: false
+    t.string "condition_id", null: false
+    t.string "postage_payer_id", null: false
+    t.string "prefecture_id", null: false
+    t.string "preparation_day_id", null: false
+    t.string "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
