40df29aa4495893f1352e245559f182771493f39
akihiro1656 <akihiro011656@yahoo.co.jp>
2024-07-25 14:49:05 +0900

    商品一覧ページから商品情報ページ遷移
     商品一覧ページにて商品情報をクリックすると、該当する商品の商品詳細表示ページへ遷移すること。

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-    devise_for :users, controllers: {
+  devise_for :users, controllers: {
       registrations: 'users/registrations'
     }
   
   root "items#index"
   get 'items/new', to: 'items#new' 
   post 'items/new', to: 'items#create'
-  get 'item/show/id', to:'item#show'
   resources :users
-  resources :items
-    
+  resources :items, only: [:index, :show, :new, :create]
 end
