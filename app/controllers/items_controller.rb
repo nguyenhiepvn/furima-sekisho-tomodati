a80ed1f0dee21a36773f775cc7a3ba8de51acc93
akihiro1656 <akihiro011656@yahoo.co.jp>
2024-07-25 13:57:02 +0900

    ItemsController修正
    商品詳細ページを誰でも見られるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,15 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @items = Item.order("created_at DESC")
   end  
   
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
@@ -16,13 +20,12 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new,locals:{item: @item}, status: :unprocessable_entity
+      render :new, locals: { item: @item }, status: :unprocessable_entity
     end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id,
-                                 :condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :image).merge(user_id: current_user.id)
   end
 end
