b5a1e6b4d501e5c411f99abbe295183183ded9fe
akihiro1656 <akihiro011656@yahoo.co.jp>
2024-07-26 11:09:47 +0900

    修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :set_item, only: [:show, :destroy]
   def index
     @items = Item.order("created_at DESC")
   end  
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -25,15 +23,20 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    if @item.user == current_user
+
+    if current_user.id == @item.user_id
       @item.destroy
-      redirect_to root_path, notice: '商品が削除されました。'
+      flash[:notice] = "商品が削除されました。"
     end
+    redirect_to root_path # 条件に関係なく、削除後はトップページにリダイレクトします
   end
   
   private
   def item_params
     params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
