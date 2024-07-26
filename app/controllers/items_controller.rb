79b614c397780219ca01309137cd33d0d8260194
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-26 16:06:43 +0900

    Revert "編集ボタン パス"
    This reverts commit c699ca915aa55ef9075f998dfacf7f4463decd2e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,11 @@
 class ItemsController < ApplicationController
-
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit]
-  before_action :set_item, only: [:edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :destroy]
   def index
     @items = Item.order("created_at DESC")
-  end
+  end  
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -25,34 +22,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    unless @item.user == current_user
-      redirect_to action: :index
-    end
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit , status: :unprocessable_entity
-    end
-  end
-
   def destroy
+
     if current_user.id == @item.user_id
       @item.destroy
       flash[:notice] = "商品が削除されました。"
     end
     redirect_to root_path # 条件に関係なく、削除後はトップページにリダイレクトします
   end
-
-  private
   
+  private
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id,
-                                 :condition_id, :postage_payer_id, :prefecture_id, 
-                                 :preparation_day_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :image).merge(user_id: current_user.id)
   end
 
   def set_item
