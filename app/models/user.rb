c699ca915aa55ef9075f998dfacf7f4463decd2e
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-26 16:03:06 +0900

    編集ボタン パス

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
