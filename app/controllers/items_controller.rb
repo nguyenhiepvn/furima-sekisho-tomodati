7e63cf869d01db38266cb65411f2bf42b2771056
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-29 09:33:34 +0900

    プルリクエスト 修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,13 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :update, :edit]
-  before_action :set_item, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :update, :destroy, :show]
 
   def index
     @items = Item.order("created_at DESC")
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
