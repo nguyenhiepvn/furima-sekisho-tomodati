674a0fd275fd75eeb31cf054b7e652128cf26f2f
nguyenhiepvn <nguyenvanhiepkenshu@gmail.com>
2024-08-05 13:31:08 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module FurimaSekishoTomodati
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :ja]
+
   end
 end
